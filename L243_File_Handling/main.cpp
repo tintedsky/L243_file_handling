3222a8c304b789c19a85485efb2bbc25eaa0cd14
tintedsky <hongbonew@gmail.com>
2022-11-28 22:44:18 -0500

    My version for ifstream.

--- L243_File_Handling/main.cpp
+++ L243_File_Handling/main.cpp
@@ -6,9 +6,39 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+void fwrite()
+{
+    std::ofstream ofs("MyText.txt", std::ios::app);
+    ofs<<"Hello"<<std::endl;
+    ofs<<255<<std::endl;
+    ofs.close();
+    return;
+}
+
+void fread()
+{
+    std::ifstream ifs;
+    ifs.open("MyText.txt");
+    if(!ifs.is_open())
+    {
+        std::cout<<"File cannot be opened"<<std::endl;
+    }
+    
+    std::string str;
+    while(!ifs.eof())
+    {
+        ifs>>str;
+        std::cout<<str<<std::endl;
+    }
+    
+    std::cout<<"end of file"<<std::endl;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    fwrite();
+    fread();
     return 0;
 }
